bcb00cf7c89bb2da09326c116501316d34977997
logandavies181 <logan.davies181@gmail.com>
2021-08-20 11:31:24 +0000

    add semver to build

--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,8 @@ var rootCmd = &cobra.Command{
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(semver string) {
+	rootCmd.Version = semver
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/logandavies181/tfd/cmd"
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+
+	"github.com/logandavies181/tfd/cmd"
+)
+
+var semver string
 
 func main() {
-	cmd.Execute()
+	debugInfo, ok := debug.ReadBuildInfo()
+	if semver == "" {
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Could not determine build info. Your tfd version is corrupt")
+			os.Exit(1)
+		}
+
+		var buildMeta string
+		if debugInfo.Main.Sum == "" {
+			buildMeta = "dev"
+		} else {
+			buildMeta = debugInfo.Main.Sum
+		}
+
+		semver = "0.0.0+"+buildMeta
+	}
+
+	cmd.Execute(semver)
 }
