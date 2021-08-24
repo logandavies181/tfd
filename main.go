e8da5b0183d1ae1fd1c10b8764e992c349a3ecea
logandavies181 <logan.davies181@gmail.com>
2021-08-24 05:32:12 +0000

    get nice tag version from debug.ReadBuildInfo

--- main.go
+++ main.go
@@ -4,21 +4,47 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
+	"strings"
 
 	"github.com/logandavies181/tfd/cmd"
 )
 
+var version string
+
 func main() {
-	debugInfo, ok := debug.ReadBuildInfo()
-	if !ok {
-		fmt.Fprintln(os.Stderr, "Could not determine build info. Your tfd version is corrupt")
-		os.Exit(1)
-	}
+	cmd.Execute(buildVersion())
+}
 
-	version := debugInfo.Main.Version
-	if version == "(devel)" {
-		version = "v0.0.0+devel"
-	}
+// buildVersion checks if version has been set at build time, otherwise uses debug.ReadBuildInfo to infer a tag.
+// debug.ReadBuildInfo behaves differently given the following scenarios so buildVersion does the following:
+// Locally compiled - Main.Version = "devel"; return "0.0.0+devel"
+// go get tfd@<some git hash> - Main.Version = "0.0.0+v0.0.0-<timestamp>-<short_hash>"; return 0.0.0-<timestamp>-<short_hash>
+// go get tfd@<tag> - Main.Version = "0.0.0-v<tag>"; return "<tag>"
+func buildVersion() string {
+	if version == "" {
+		debugInfo, ok := debug.ReadBuildInfo()
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Could not determine build info. Your tfd version is corrupt")
+			os.Exit(1)
+		}
 
-	cmd.Execute(version)
+		version := debugInfo.Main.Version
+		if version == "(devel)" {
+			return "0.0.0+devel"
+		}
+
+		foundVersionStart := false
+		version = strings.TrimLeftFunc(version, func(r rune) bool {
+			if r == 'v' {
+				// only false for first v - trim it but not the others
+				if foundVersionStart {
+					return false
+				}
+				foundVersionStart = true
+
+			}
+			return !foundVersionStart
+		})
+	}
+	return version
 }
