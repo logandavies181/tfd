f0986f9ccdebfa30a7c2647f41a0bd3853f37fe3
logandavies181 <logan.davies181@gmail.com>
2021-09-08 06:20:44 +0000

    checkout tag in release.sh

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ set -o nounset
 set -o pipefail
 
 VERSION="$1"
-TAG_VERSION="v${VERSION}"
 TARBALL="tfd_${VERSION}_linux_amd64.tgz"
 
+git checkout "${VERSION}"
 GOOS=linux GOARCH=amd64 go build  -ldflags "-X 'main.version=${VERSION}'"
 tar -zcvf "${TARBALL}" tfd
 
-gh release create "${TAG_VERSION}" "${TARBALL}"
+gh release create "${TAG_VERSION}" "${TARBALL}" --prerelease
