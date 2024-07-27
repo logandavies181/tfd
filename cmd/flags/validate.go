86f16587155d0384ee5de7095b21d488176cf817
logandavies181 <39316073+logandavies181@users.noreply.github.com>
2024-07-27 18:50:09 +1200

    bind flags in cmd init (#36)
    Co-authored-by: logandavies181 <logie@DESKTOP-BLFJQ75>

--- cmd/flags/validate.go
+++ cmd/flags/validate.go
@@ -1,9 +1,12 @@
 package flags
 
 import (
+	"fmt"
+
 	"github.com/logandavies181/tfd/v2/cmd/config"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -29,15 +32,20 @@ func addValidation(name string, validationFunc func() error) {
 	}
 }
 
-func InitializeCmd(cmd *cobra.Command) (conf config.Config, err error) {
-	conf, err = config.New()
+func InitializeCmd(cmd *cobra.Command) (config.Config, error) {
+	conf, err := config.New()
 	if err != nil {
-		return
+		return config.Config{}, fmt.Errorf("could not create new config object: %w", err)
+	}
+
+	err = viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return config.Config{}, fmt.Errorf("could not bind flags: %w", err)
 	}
 
 	err = validateFlags(cmd.Name())
 	if err != nil {
-		return
+		return config.Config{}, err // just let the validation error display as-is
 	}
 
 	return conf, nil
